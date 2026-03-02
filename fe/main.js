dfee1ac71b41ca71616c235cd3dd03957b650622
OJIEG65 <OJIEG65@gmail.com>
2026-03-02 17:07:18 +0200

    add car road controll models, basic physics

--- fe/main.js
+++ fe/main.js
@@ -1,25 +1,25 @@
+import { Car } from "./models/car.js";
+import { Road } from "./models/road.js";
+
 const canvas = document.getElementById("canvas");
+
+canvas.width = 200;
+canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 
-function resize() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+const road = new Road(canvas.width / 2, canvas.width, 3);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+
+animate();
+
+function animate() {
+  canvas.height = window.innerHeight;
+  car.update();
+
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.7);
+
+  road.draw(ctx);
+  car.draw(ctx);
+  requestAnimationFrame(animate);
 }
-
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // placeholder
-    ctx.fillStyle = "#333";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#fff";
-    ctx.font = "24px monospace";
-    ctx.textAlign = "center";
-    ctx.fillText("CarGoAi - ready", canvas.width / 2, canvas.height / 2);
-
-    requestAnimationFrame(draw);
-}
-
-window.addEventListener("resize", resize);
-resize();
-draw();
---
+++ fe/models/car.js
@@ -0,0 +1,83 @@
+import { Controls } from "./controls.js";
+
+export class Car {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+
+    this.speed = 0;
+    this.acceleration = 0.2;
+    this.maxSpeed = 3;
+    this.friction = 0.05;
+    this.angle = 0;
+
+    this.controls = new Controls();
+  }
+
+  update() {
+
+    // basic
+    if (this.controls.forward) {
+      this.speed += this.acceleration;
+    }
+
+    if (this.controls.reverse) {
+      this.speed -= this.acceleration;
+    }
+
+    if (this.speed > this.maxSpeed) { // max speed capped
+      this.speed = this.maxSpeed;
+    }
+
+    if (this.speed < -this.maxSpeed / 2) { //reverse lover speed
+      this.speed = -this.maxSpeed / 2;
+    }
+
+    if( this.speed > 0) { // friction physics to stop the car eventually
+      this.speed -= this.friction;
+    }
+
+    if( this.speed < 0) { // friction physics to stop the car eventually from moving backwards
+      this.speed += this.friction;
+    }
+
+
+    if(this.speed!==0) {
+        const flip = this.speed > 0 ? 1 : -1;
+
+      // left / right controls
+      if( this.controls.left) {
+        this.angle += 0.03 * flip;
+      }
+
+      if(this.controls.right) {
+        this.angle -= 0.03 * flip;
+      }
+    }
+
+
+
+
+    // math to push the car on coordinates to follow vector path
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cosh(this.angle) * this.speed;
+  }
+
+  draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
+
+    ctx.beginPath();
+    ctx.rect(
+      -this.width / 2,
+      -this.height / 2,
+      this.width,
+      this.height
+    );
+    ctx.fill();
+    ctx.restore();
+  }
+}
